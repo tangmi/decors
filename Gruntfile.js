caca18087c1ceb539cfd7bf2081327466880c2c8
Michael Tang <tangmi@uw.edu>
2013-06-21 23:00:16 -0700

    Initial commit and basic functionality

---
+++ Gruntfile.js
@@ -0,0 +1,111 @@
+module.exports = function(grunt) {
+
+	var http = require('http');
+	var https = require('https');
+
+	var urlPrefix = '_decors';
+	var decors = function(req, res, next) {
+		var path = req.originalUrl;
+		if (path.match(urlPrefix)) {
+			var url = path.substring(('/' + urlPrefix + '/').length);
+
+			var regex = new RegExp('^([a-zA-Z]+)://([^/]+)(/.*)?$');
+			var protocol = url.replace(regex, '$1') == 'https' ? https : http;
+			var restRequest = protocol.request({
+				host: url.replace(regex, '$2'),
+				path: url.replace(regex, '$3'),
+				type: req.method
+			}, function(data) {
+
+				var body = '';
+
+				data.on('data', function(chunk) {
+					body += chunk;
+				});
+
+				data.on('end', function() {
+					res.statusCode = data.statusCode;
+					res.headers = data.headers;
+					res.end(body);
+				});
+
+			}).on('error', function(e) {
+				res.end(JSON.stringify({
+					'status': 'error',
+					'message': e
+				}, undefined, 2));
+			});
+
+			req.content = '';
+			req.addListener('data', function(chunk) {
+				req.content += chunk;
+			});
+			req.addListener('end', function() {
+				restRequest.headers = req.headers;
+				restRequest.write(req.content);
+				restRequest.end();
+			});
+
+		} else {
+			next();
+		}
+
+		var time = ('[' + (new Date).toLocaleTimeString() + ']');
+
+		grunt.log.writeln('').write(time.grey + ' ' + res.statusCode.toString().yellow + ' ' + req.originalUrl);
+	};
+
+	var LIVERELOAD_PORT = 35729;
+
+	var livereload = require('connect-livereload')({
+		port: LIVERELOAD_PORT
+	});
+
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	grunt.initConfig({
+		watch: {
+			options: {
+				nospawn: true
+			},
+			livereload: {
+				options: {
+					livereload: LIVERELOAD_PORT
+				},
+				files: [
+						'app/**/*',
+				]
+			}
+		},
+		connect: {
+			options: {
+				port: 9000,
+				hostname: 'localhost'
+			},
+			livereload: {
+				options: {
+					middleware: function(connect) {
+						var middleware = [];
+						middleware[0] = decors;
+						middleware[1] = livereload;
+						middleware[2] = connect.static(require('path').resolve('app'));
+						return middleware;
+					}
+				}
+			}
+		},
+		open: {
+			server: {
+				path: 'http://localhost:<%= connect.options.port %>'
+			}
+		}
+	});
+	grunt.registerTask('default', [
+			'connect:livereload',
+			// 'open:server',
+			'watch:livereload'
+	]);
+	grunt.event.on('watch', function(action, filepath) {
+		grunt.log.writeln('');
+	});
+};
---
+++ README.md
@@ -0,0 +1,16 @@
+# decors
+
+This is a response to the frustation from running into CORS issues in cross-domain ajax calls. Decors simply acts as a proxy for you while you begin the development of your webapp (you probably don't want to use this when you go to production).
+
+## Why?
+
+Perhaps if you are co-developing with a partner and he/she is working on the backend. If you are on the same network, you can access their IP address and pick up their changes in real-time. It's like CO-OP development.
+
+Decors also speeds up your development marginally by providing middleware that reloads your page in the browser as you make changes to it.
+
+## How to run
+
+There's two ways of running decors. Go into the `./decors` directory and run `npm install`, then pick one:
+
+ * Run `npm start` (or `./node_modules/.bin/grunt`)
+ * Install `grunt-cli`, then run `grunt`.
