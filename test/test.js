3cd5116ccc91c9320239643b2cb7605146179f19
Michael Tang <tangmi@uw.edu>
2013-08-04 00:11:30 -0700

    Updated some tests

--- test/test.js
+++ test/test.js
@@ -5,47 +5,75 @@ var assert = require('assert'),
 	request = require('supertest'),
 	path = require('path');
 
-var testapp = express();
-
-testapp.set('port', 9001);
-
-testapp.use(express.bodyParser());
-
-testapp.get('/remote/get/:number', function(req, res) {
-	res.send(req.param('number'));
-});
-
-testapp.post('/remote/post', function(req, res) {
-	res.send(req.param('number'));
-});
-
-
-
-http.createServer(testapp).listen(testapp.get('port'), function() {});
-
-
-before(function(done) {
-	decors.start({
-		dir: path.join(__dirname),
-		backend: 'localhost:9001',
-		port: 9000,
-		quiet: true
-	}, function() {
-		done();
-	});
-});
-
-describe('Routing', function() {
+describe('Static server', function() {
 	var base = 'http://localhost:9000';
 
-	describe('Static server', function() {
-		it('should serve static files', function(done) {
-			request(decors.getExpressInstance())
-				.get('/get.html')
-				.expect('get static', done);
+	before(function(done) {
+		decors.start({
+			dir: path.join(__dirname),
+			port: 9000,
+			quiet: true
+		}, function() {
+			done();
 		});
 	});
+
+	it('should serve static files', function(done) {
+		request(decors.getExpressInstance())
+			.get('/local.html')
+			.expect('get static', done);
+	});
+
+	it('should fail when no file is found', function(done) {
+		request(decors.getExpressInstance())
+			.get('/get2.html')
+			.expect(404, done);
+	});
+
+	after(function(done) {
+		decors.stop();
+		done();
+	});
+
+});
+
+describe('Backend configuration', function() {
+
+	before(function(done) {
+		decors.start({
+			dir: path.join(__dirname),
+			backend: 'localhost:9001',
+			port: 9000,
+			quiet: true
+		}, function() {
+			done();
+		});
+	});
+
 	describe('Remote methods', function() {
+		var server = null;
+		before(function(done) {
+			var testapp = express();
+			testapp.set('port', 9001);
+			testapp.use(express.bodyParser());
+			testapp.get('/remote/get/:number', function(req, res) {
+				res.send(req.param('number'));
+			});
+			testapp.post('/remote/post', function(req, res) {
+				res.send(req.param('number'));
+			});
+			testapp.get('/remote/img/binary.png', function(req, res) {
+				// res.setHeader('Content-Encoding', 'gzip');
+				res.setHeader('Content-Type', 'image/png');
+				res.send('decors');
+			});
+
+			server = http.createServer(testapp);
+			server.listen(testapp.get('port'), function() {
+				done();
+			});
+		});
+
 		it('should grab remote GET requests', function(done) {
 			var num = "5";
 			request(decors.getExpressInstance())
@@ -62,5 +90,58 @@ describe('Routing', function() {
 				})
 				.expect(num, done);
 		});
+
+		it('should correctly download binary files', function(done) {
+			request(decors.getExpressInstance())
+				.get('/remote/img/binary.png')
+				.expect('Content-Type', 'image/png')
+				.expect('decors', done);
+		});
+
+		after(function(done) {
+			server.close();
+			done();
+		});
 	});
+
+	describe('Gzip passthrough', function() {
+		var server = null;
+		before(function(done) {
+			var zlib = require('zlib');
+
+			var testapp = express();
+			testapp.set('port', 9001);
+			testapp.use(express.bodyParser());
+
+			testapp.get('/remote/gzip', function(req, res) {
+				res.setHeader('Content-Encoding', 'gzip');
+				res.setHeader('Content-Type', 'text/plain');
+				zlib.gzip('decors', function(err, data) {
+					res.send(data);
+				});
+			});
+
+			server = http.createServer(testapp);
+			server.listen(testapp.get('port'), function() {
+				done();
+			});
+		});
+
+		it('should grab remote GET requests', function(done) {
+			request(decors.getExpressInstance())
+				.get('/remote/gzip')
+				.expect('decors', done);
+		});
+
+		after(function(done) {
+			server.close();
+			done();
+		});
+	});
+
+	after(function(done) {
+		decors.stop();
+		done();
+	});
+
 });
